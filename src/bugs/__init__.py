998c347d8e0df3c421b95141c5d9c72ff82946e4
thcrt <110127860+thcrt@users.noreply.github.com>
2025-05-31 16:53:47 +0200

    Allow building with Nuitka

--- src/bugs/__init__.py
+++ src/bugs/__init__.py
@@ -3,6 +3,6 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 
-from ._version import __version__
+from ._version import __version__, __version_tuple__
 
-__all__ = ["__version__", ]
+__all__ = ["__version__", "__version_tuple__"]
